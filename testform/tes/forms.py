07899c0c91fb553868b9b020be0eccebec3a7383
vitiminA <kikipham1008@gmail.com>
2023-11-23 16:18:07 +0700

    kế thừa các ttrang html

--- testform/tes/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class smart_Form(forms.Form):
-    title = forms.CharField(max_length=255)
-    image = forms.ImageField()
-    body = forms.CharField(widget= forms.Textarea)
--- testform/tes/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .import views
-
-app_name = 'smart'
-urlpatterns = [
-    path("", views.smart, name='smart'),
-    path("savesmart/", views.savesmart, name='savesmart'),
-]
